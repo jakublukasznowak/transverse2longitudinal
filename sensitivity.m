21ae3c0c3d2ad23bab43f4ca51dd247f77460b33
jakublukasznowak <jakublukasznowak@gmail.com>
2024-05-07 17:31:23 +0200

    Last edits to sensitivity script

--- sensitivity.m
+++ sensitivity.m
@@ -97,12 +97,12 @@ for i_p = 1:Npl
         
         % Structure functions
         
-        fprintf('%s','SFC ... ')
+        fprintf('%s','  SFC ... ')
 
         MOM.sfc_fit_range = [MOM.dr*sfc_bot_factors(i_p) MOM.int_scale*sfc_upp_factors(i_f)];
         
         for i_v = 1:Nvar
-            var = vars{i_v}; fprintf('%3s',var)
+            var = vars{i_v}; fprintf(' %s',var)
             
             MOM{:,["off","slp","e_off","e_slp","R2","N"]+"_sfc_"+var} = nan;
             
@@ -135,12 +135,12 @@ for i_p = 1:Npl
         
         % Power spectra
         
-        fprintf('%s','PSD ... ')
+        fprintf('%s','  PSD ... ')
         
         MOM.psd_fit_range = [MOM.dr*psd_bot_factors(i_p) MOM.int_scale*psd_upp_factors(i_f)];
         
         for i_v = 1:Nvar
-            var = vars{i_v}; fprintf('%3s',var)
+            var = vars{i_v}; fprintf(' %s',var)
             
             MOM{:,["off","slp","e_off","e_slp","R2","N"]+"_psd_"+var} = nan;
             
@@ -298,18 +298,18 @@ end
 
 % Printout a piece of a jam script
 
-jamplots = {'ar_uv','ar_uw','slp'};
-for i_j = 1:numel(jamplots)
-    fprintf('pdfjam --nup %dx%d --papersize ''{%dcm,%dcm}''',Npl,Nfc,Npl*16,Nfc*12)
-    for i_f = 1:Nfc
-        testname = sprintf('sfc %.1fL psd %.1fL',sfc_upp_factors(i_f),psd_upp_factors(i_f));
-        fprintf(' ./%s/*%s.png',replace(testname,{'.',' ',',','/'},{'p','_','',''}),jamplots{i_j})
-    end
-    fprintf(' --outfile ./jam/%s.pdf\n',jamplots{i_j})
-end
-fprintf('pdfjam --papersize ''{%dcm,%dcm}''',Npl*16,Nfc*12)
-fprintf(' ./jam/%s.pdf',jamplots{:})
-fprintf(' --outfile ./jam/scatter.pdf\n')
+% jamplots = {'ar_uv','ar_uw','slp'};
+% for i_j = 1:numel(jamplots)
+%     fprintf('pdfjam --nup %dx%d --papersize ''{%dcm,%dcm}''',Npl,Nfc,Npl*16,Nfc*12)
+%     for i_f = 1:Nfc
+%         testname = sprintf('sfc %.1fL psd %.1fL',sfc_upp_factors(i_f),psd_upp_factors(i_f));
+%         fprintf(' ./%s/*%s.png',replace(testname,{'.',' ',',','/'},{'p','_','',''}),jamplots{i_j})
+%     end
+%     fprintf(' --outfile ./jam/%s.pdf\n',jamplots{i_j})
+% end
+% fprintf('pdfjam --papersize ''{%dcm,%dcm}''',Npl*16,Nfc*12)
+% fprintf(' ./jam/%s.pdf',jamplots{:})
+% fprintf(' --outfile ./jam/scatter.pdf\n')
 
         
 
@@ -317,8 +317,7 @@ fprintf(' --outfile ./jam/scatter.pdf\n')
 
 sum_vars = {'ar_sfc_VU','ar_psd_VU','ar_sfc_WU','ar_psd_WU',...
     'slp_sfc_UX','slp_sfc_VY','slp_sfc_W',...
-    'slp_psd_UX','slp_psd_VY','slp_psd_W',...
-    'R2_sfc_tot','R2_psd_tot','R2_tot_tot',...
+    'slp_psd_UX','slp_psd_VY','slp_psd_W',...    'R2_sfc_tot','R2_psd_tot','R2_tot_tot',...
     'N_sfc_UX','N_psd_UX'};
 
 grp_vars = {'testname','sfc_upp_factor','psd_upp_factor','plane'};
@@ -343,9 +342,9 @@ for i_p = 1:Npl
         MOM_matrix{i_p,i_f}.sfc_upp_factor(:) = sfc_upp_factors(i_f);
         MOM_matrix{i_p,i_f}.psd_upp_factor(:) = psd_upp_factors(i_f);
         
-        MOM_matrix{i_p,i_f}.R2_sfc_tot = MOM_matrix{i_p,i_f}.R2_sfc_UX .* MOM_matrix{i_p,i_f}.R2_sfc_VY .* MOM_matrix{i_p,i_f}.R2_sfc_W;
-        MOM_matrix{i_p,i_f}.R2_psd_tot = MOM_matrix{i_p,i_f}.R2_psd_UX .* MOM_matrix{i_p,i_f}.R2_psd_VY .* MOM_matrix{i_p,i_f}.R2_psd_W;
-        MOM_matrix{i_p,i_f}.R2_tot_tot = MOM_matrix{i_p,i_f}.R2_sfc_tot .* MOM_matrix{i_p,i_f}.R2_psd_tot;
+%         MOM_matrix{i_p,i_f}.R2_sfc_tot = MOM_matrix{i_p,i_f}.R2_sfc_UX .* MOM_matrix{i_p,i_f}.R2_sfc_VY .* MOM_matrix{i_p,i_f}.R2_sfc_W;
+%         MOM_matrix{i_p,i_f}.R2_psd_tot = MOM_matrix{i_p,i_f}.R2_psd_UX .* MOM_matrix{i_p,i_f}.R2_psd_VY .* MOM_matrix{i_p,i_f}.R2_psd_W;
+%         MOM_matrix{i_p,i_f}.R2_tot_tot = MOM_matrix{i_p,i_f}.R2_sfc_tot .* MOM_matrix{i_p,i_f}.R2_psd_tot;
     end
 end
 
@@ -426,35 +425,35 @@ print(fig,[plotpath_sum,filesep,'slp_w'],'-dpng','-r300')
 
 % R2
 
-fig = plot_xy_uni(SUM,{'mean_R2_psd_tot'},{'mean_R2_sfc_tot'},...
-    'testname','plane',[],true,{'cross1'},mks);
-xlabel('$R_{Pu}^2R_{Pv}^2R_{Pw}^2$','Interpreter','latex')
-ylabel('$R_{Du}^2R_{Dv}^2R_{Dw}^2$','Interpreter','latex')
-print(fig,[plotpath_sum,filesep,'R2_tot'],'-dpng','-r300')
-
-[fig,~,co] = fig16x12;
-for i_p = 1:Npl
-    ind = SUM.plane==planes{i_p};
-    plot(SUM.sfc_upp_factor(ind),SUM.mean_R2_tot_tot(ind),'-o','Color',co(i_p,:))
-end
-legend(planes,'Location','best')
-xlabel('$F$ sfc','Interpreter','latex')
-ylabel('$R_{Du}^2R_{Dv}^2R_{Dw}^2R_{Pu}^2R_{Pv}^2R_{Pw}^2$','Interpreter','latex')
-print(fig,[plotpath_sum,filesep,'R2_tot_tot'],'-dpng','-r300')
+% fig = plot_xy_uni(SUM,{'mean_R2_psd_tot'},{'mean_R2_sfc_tot'},...
+%     'testname','plane',[],true,{'cross1'},mks);
+% xlabel('$R_{Pu}^2R_{Pv}^2R_{Pw}^2$','Interpreter','latex')
+% ylabel('$R_{Du}^2R_{Dv}^2R_{Dw}^2$','Interpreter','latex')
+% print(fig,[plotpath_sum,filesep,'R2_tot'],'-dpng','-r300')
+% 
+% [fig,~,co] = fig16x12;
+% for i_p = 1:Npl
+%     ind = SUM.plane==planes{i_p};
+%     plot(SUM.sfc_upp_factor(ind),SUM.mean_R2_tot_tot(ind),'-o','Color',co(i_p,:))
+% end
+% legend(planes,'Location','best')
+% xlabel('$F$ sfc','Interpreter','latex')
+% ylabel('$R_{Du}^2R_{Dv}^2R_{Dw}^2R_{Pu}^2R_{Pv}^2R_{Pw}^2$','Interpreter','latex')
+% print(fig,[plotpath_sum,filesep,'R2_tot_tot'],'-dpng','-r300')
 
 
 % Rejected segments
 
-CNTrej = CNT;
-CNTrej.GroupCount(CNTrej.N_sfc_UX>=sfc_min_fit_points) = 0;
-CNTrej = groupsummary(CNTrej,{'sfc_upp_factor','plane'},{'sum'},{'GroupCount'});
-
-fig = fig16x12;
-for i_p = 1:Npl
-    ind = CNTrej.plane==planes{i_p};
-    plot(CNTrej.sfc_upp_factor(ind),CNTrej.sum_GroupCount(ind),'-o')
-end
-legend(planes,'Location','best')
-xlabel('$F$ sfc','Interpreter','latex')
-ylabel('# rejected segments')
-print(fig,[plotpath_sum,filesep,'rej_tot'],'-dpng','-r300')
+% CNTrej = CNT;
+% CNTrej.GroupCount(CNTrej.N_sfc_UX>=sfc_min_fit_points) = 0;
+% CNTrej = groupsummary(CNTrej,{'sfc_upp_factor','plane'},{'sum'},{'GroupCount'});
+% 
+% fig = fig16x12;
+% for i_p = 1:Npl
+%     ind = CNTrej.plane==planes{i_p};
+%     plot(CNTrej.sfc_upp_factor(ind),CNTrej.sum_GroupCount(ind),'-o')
+% end
+% legend(planes,'Location','best')
+% xlabel('$F$ sfc','Interpreter','latex')
+% ylabel('# rejected segments')
+% print(fig,[plotpath_sum,filesep,'rej_tot'],'-dpng','-r300')
